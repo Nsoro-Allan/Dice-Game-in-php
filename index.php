03e84670787633bf86374f47c6b89f413fcd2d13
allancorp23 <allancorp250@gmail.com>
2023-12-25 17:02:48 +0200

    changes done

--- index.php
+++ index.php
@@ -70,6 +70,7 @@ if ($score1 >=20 || $score2 >=20){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dice Game</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./Dice Images/icon.png" type="image/x-icon">
 </head>
 <body>
     <div class="container">
